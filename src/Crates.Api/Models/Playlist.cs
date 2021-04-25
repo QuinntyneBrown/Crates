ed09ac5122c6fac927179dc82196ba3d002b3f82
QuinntyneBrown <quinntynebrown@gmail.com>
2021-04-24 23:18:20 -0400

    update

--- src/Crates.Api/Models/Playlist.cs
+++ src/Crates.Api/Models/Playlist.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 namespace Crates.Api.Models
 {
     public class Playlist
     {
         public Guid PlaylistId { get; set; }
+        public List<Song> Songs { get; private set; } = new();
     }
 }
--- src/Crates.Api/Models/Song.cs
+++ src/Crates.Api/Models/Song.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 namespace Crates.Api.Models
 {
     public class Song
     {
         public Guid SongId { get; set; }
+        public List<Song> Songs { get; private set; } = new();
     }
 }
