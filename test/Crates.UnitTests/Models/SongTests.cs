26eafd1c7dea23ae937c6d13d7ebf2a34628b96b
QuinntyneBrown <quinntynebrown@gmail.com>
2021-04-26 03:40:03 -0400

    update

--- test/Crates.UnitTests/Models/SongTests.cs
+++ test/Crates.UnitTests/Models/SongTests.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Crates.Api.Models;
+using Xunit;
 
-namespace Crates.UnitTests.Models
+namespace Crates.UnitTests
 {
-    class SongTests
+    public class SongTests
     {
+        [Fact]
+        public void Constuctor()
+        {
+            var actual = CreateSong();
+        }
+        
+        public Song CreateSong()
+        {
+            return new();
+        }
+        
     }
 }
